16394aa6a47331998436a7180837f7b36ca04afa
DaWrecka <paul.waldron@blueyonder.co.uk>
2020-11-17 09:58:57 +0000

    Revert "First public release"
    This reverts commit 4656eaccd51d57c400ba4bc86d0cc6e16184df25.

--- AcidProofSuit/Main.cs
+++ AcidProofSuit/Main.cs
@@ -2,7 +2,6 @@
 using System.Reflection;
 using HarmonyLib;
 using QModManager.API.ModLoading;
-using Logger = QModManager.Utility.Logger;
 
 namespace AcidProofSuit
 {
@@ -11,16 +10,9 @@ namespace AcidProofSuit
     {
         public static bool bInAcid = false; // Whether or not the player is currently immersed in acid
 
-        internal static AcidSuitPrefab                      suitPrefab =        new AcidSuitPrefab();
-        internal static AcidGlovesPrefab                    glovesPrefab =      new AcidGlovesPrefab();
-        internal static AcidHelmetPrefab                    helmetPrefab =      new AcidHelmetPrefab();
-        internal static bpSupplemental_OnlyRadSuit          bpOnlyRadSuit =     new bpSupplemental_OnlyRadSuit();
-        internal static bpSupplemental_OnlyRebreather       bpOnlyRebreather =  new bpSupplemental_OnlyRebreather();
-        internal static bpSupplemental_OnlyReinforcedSuit   bpOnlyReinforced =  new bpSupplemental_OnlyReinforcedSuit();
-        internal static bpSupplemental_Suits                bpSuits =           new bpSupplemental_Suits();
-        internal static bpSupplemental_RebreatherRad        bpRebreatherRad =   new bpSupplemental_RebreatherRad();
-        internal static bpSupplemental_RebreatherReinforced bpRebReinf =        new bpSupplemental_RebreatherReinforced();
-        internal static bpSupplemental_RadReinforced        bpRadReinf =        new bpSupplemental_RadReinforced();
+        internal static AcidSuitPrefab suitPrefab = new AcidSuitPrefab();
+        internal static AcidGlovesPrefab glovesPrefab = new AcidGlovesPrefab();
+        internal static AcidHelmetPrefab helmetPrefab = new AcidHelmetPrefab();
 
         // This function was stol*cough*take*cough*nicked wholesale from FCStudios
         public static object GetPrivateField<T>(this T instance, string fieldName, BindingFlags bindingFlags = BindingFlags.Default)
@@ -28,99 +20,13 @@ namespace AcidProofSuit
             return typeof(T).GetField(fieldName, BindingFlags.Instance | BindingFlags.NonPublic | bindingFlags).GetValue(instance);
         }
 
-        internal struct DamageInfo
-        {
-            public DamageType damageType;
-            public float damageMult;
-
-            public DamageInfo(DamageType t, float m)
-            {
-                this.damageType = t;
-                this.damageMult = m;
-            }
-        }
-
-        internal struct DamageResistance
-        {
-            public TechType TechType;
-            public DamageInfo[] damageInfoList;
-
-            public DamageResistance(TechType tt, DamageInfo[] dil)
-            {
-                this.TechType = tt;
-                this.damageInfoList = dil;
-            }
-        }
-
-        // This particular system is not that useful, but it could be expanded to allow any sort of equipment type to reduce damage.
-        // For example, you could add a chip that projects a sort of shield that protects from environmental damage, such as Acid, Radiation, Heat, Poison, or others.
-        // Although the system would need to be extended to allow, say, a shield that drains a battery when resisting damage.
-        internal static DamageResistance[] DamageResistances;
-        public static float ModifyDamage(TechType tt, float damage, DamageType type)
-        {
-            float baseDamage = damage;
-            float damageMod = 0;
-            //Logger.Log(Logger.Level.Debug, $"Main.ModifyDamage called: tt = {tt.ToString()}, damage = {damage}; DamageType = {type}");
-            foreach (DamageResistance r in DamageResistances)
-            {
-                //Logger.Log(Logger.Level.Debug, $"Found DamageResistance with TechType: {r.TechType.ToString()}");
-                if (r.TechType == tt)
-                {
-                    foreach (DamageInfo d in r.damageInfoList)
-                    {
-                        if (d.damageType == type)
-                        {
-                            damageMod += baseDamage * d.damageMult;
-                            //Logger.Log(Logger.Level.Debug, $"Player has equipped armour of TechType {tt.ToString()}, base damage = {baseDamage}, type = {type}, modifying damage by {d.damageMult}x with result of {damageMod}");
-                        }
-                    }
-                }
-            }
-            return damageMod;
-        }
-
         [QModPatch]
         public static void Load()
         {
+            
             glovesPrefab.Patch();
             helmetPrefab.Patch();
-            // The gloves and helmet are used in the Suit recipe as Linked Items, and they must be patched before the suit.
             suitPrefab.Patch();
-            bpOnlyRadSuit.Patch();
-            bpOnlyRebreather.Patch();
-            bpOnlyReinforced.Patch();
-            bpSuits.Patch();
-            bpRebreatherRad.Patch();
-            bpRebReinf.Patch();
-            bpRadReinf.Patch();
-
-            Main.DamageResistances = new DamageResistance[3] {
-            // Gloves
-                new DamageResistance(
-                    Main.glovesPrefab.TechType,
-                    new DamageInfo[] {
-                        new DamageInfo(DamageType.Acid, -0.15f)/*,
-                        new DamageInfo(DamageType.Radiation, -0.10f)*/
-                    }),
-
-
-            // Helmet
-                new DamageResistance(
-                    Main.helmetPrefab.TechType,
-                    new DamageInfo[] {
-                        new DamageInfo(DamageType.Acid, -0.25f)/*,
-                        new DamageInfo(DamageType.Radiation, -0.20f)*/
-                    }),
-
-
-            // Suit
-                new DamageResistance(
-                    Main.suitPrefab.TechType,
-                    new DamageInfo[] {
-                        new DamageInfo(DamageType.Acid, -0.6f)/*,
-                        new DamageInfo(DamageType.Radiation, -0.70f)*/
-                    })
-            };
 
             Assembly assembly = Assembly.GetExecutingAssembly();
             new Harmony($"DaWrecka_{assembly.GetName().Name}").PatchAll(assembly);
--- AcidProofSuit/Patches/Patches.cs
+++ AcidProofSuit/Patches/Patches.cs
@@ -13,42 +13,36 @@ using SMLHelper.V2.Utility;
 
 namespace AcidProofSuit.Patches
 {
+
     [HarmonyPatch(typeof(Equipment), nameof(Equipment.GetCount))]
     internal class Equipment_GetCount_Patch
     {
-        // This patch allows for "substitutions", as it were; Specifically, it allows the modder to set up certain TechTypes to return results for both itself and another type.
-        // For example, the initial purpose was to allow requests for the Rebreather to return a positive result if the Acid Helmet were worn.
-
-        internal static Dictionary<TechType, TechType> Substitutions = new Dictionary<TechType, TechType>
-        {
-            // The key is the TT to search for, and the value is the TT to return a positive value for.
-            // so for key "TechType.Rebreather" and value "TechType.AcidHelmet", if GetCount(Rebreather) is called, the function will add one if the AcidHelmet is equipped.
-            { TechType.Rebreather, Main.helmetPrefab.TechType },
-            { TechType.RadiationHelmet, Main.helmetPrefab.TechType },
-            { TechType.RadiationSuit, Main.suitPrefab.TechType },
-            { TechType.RadiationGloves, Main.glovesPrefab.TechType }
-        };
-
+        // This patch is specifically for the Rebreather; specifically the Acid Helmet.
+        // If the function requests the Rebreather, we'll also check for the Acid Helmet
         [HarmonyPostfix]
         public static void PostFix(ref Equipment __instance, ref int __result, TechType techType)
         {
+            //int result;
+            // FieldInfo f = __instance.GetType().GetField("equippedCount");
+            //Dictionary<TechType, int> equipCount = (Dictionary<TechType, int>)(__instance.GetPrivateField("equippedCount", BindingFlags.NonPublic | BindingFlags.Instance));
+
+            /*MethodInfo dynMethod = __instance.GetType().GetMethod("UpdateReinforcedSuit", BindingFlags.NonPublic | BindingFlags.Instance);
+            dynMethod.Invoke(__instance, null);*/
+
+            //this.equippedCount.TryGetValue(techType, out result);
+
             Dictionary<TechType, int> equipCount = __instance.GetInstanceField("equippedCount", BindingFlags.NonPublic | BindingFlags.Instance) as Dictionary<TechType, int>;
             // equipCount.TryGetValue(techType, out result);
 
-            //Logger.Log(Logger.Level.Debug, $"Equipment_GetCount_Patch: techType = {techType.ToString()}, __result = {__result}");
-            //if (techType == TechType.Rebreather)
-            if(Substitutions.TryGetValue(techType, out TechType sub))
-            //foreach (KeyValuePair<TechType, TechType> sub in Substitutions)
+            Logger.Log(Logger.Level.Debug, $"Equipment_GetCount_Patch: techType = {techType.ToString()}, __result = {__result}");
+            if (techType == TechType.Rebreather)
             {
-                //if (techType == sub.Key)
-                //{
-                    int i;
-                    if (equipCount.TryGetValue(sub, out i))
-                    {
-                        //Logger.Log(Logger.Level.Debug, $"Equipment_GetCount_Patch: found {techType.ToString()} equipped");
-                        __result++;
-                    }
-                //}
+                int i;
+                if (equipCount.TryGetValue(Main.helmetPrefab.TechType, out i))
+                {
+                    Logger.Log(Logger.Level.Debug, $"Equipment_GetCount_Patch: found AcidHelmet equipped");
+                    __result++;
+                }
             }
         }
     }
@@ -65,7 +59,6 @@ namespace AcidProofSuit.Patches
             if (target == Player.main.gameObject)
             {
                 float baseDamage = damage;
-                float newDamage = damage;
 
                 if (type == DamageType.Acid)
                 {
@@ -77,7 +70,7 @@ namespace AcidProofSuit.Patches
                         return 0f;
                     }
 
-                    /*
+
                     if (Inventory.main.equipment.GetCount(Main.suitPrefab.TechType) > 0)
                     {
                         damage -= baseDamage * 0.7f;
@@ -96,9 +89,9 @@ namespace AcidProofSuit.Patches
                     //    Logger.Log(Logger.Level.Debug, $"Acid damage reduced to {damageInfo.damage}");
                     //}
                     if (damage > 0f && !(Player.main.acidLoopingSound.playing))
-                        Player.main.acidLoopingSound.Play();*/
+                        Player.main.acidLoopingSound.Play();
                 }
-                /*else if(type == DamageType.Radiation)
+                else if(type == DamageType.Radiation)
                 {
                     if (Inventory.main.equipment.GetCount(Main.suitPrefab.TechType) > 0)
                     {
@@ -111,33 +104,9 @@ namespace AcidProofSuit.Patches
                         damage -= baseDamage * 0.30f;
                         //Logger.Log(Logger.Level.Debug, $"Acid damage reduced to {damage}");
                     }
-                }*/
-                Equipment equipment = Inventory.main.equipment;
-                string[] slots = new string[]
-                {
-                    "Head",
-                    "Body",
-                    "Gloves",
-                    "Foots", // Seriously? 'Foots'?
-                    "Chip1",
-                    "Chip2",
-                    "Tank"
-                };
-                Player __instance = Player.main;
-                //int num = __instance.equipmentModels.Length;
-                //for (int i = 0; i < num; i++)
-                foreach(string s in slots)
-                {
-                    //Player.EquipmentType equipmentType = __instance.equipmentModels[i];
-                    //TechType techTypeInSlot = equipment.GetTechTypeInSlot(equipmentType.slot);
-                    TechType techTypeInSlot = equipment.GetTechTypeInSlot(s);
-
-                    damage += Main.ModifyDamage(techTypeInSlot, baseDamage, type);
-                    //Logger.Log(Logger.Level.Debug, $"Found techTypeInSlot {techTypeInSlot.ToString()}; damage altered to {damage}");
                 }
             }
-
-            return damage;
+            return System.Math.Max(damage, 0f);
         }
     }
 
@@ -148,41 +117,37 @@ namespace AcidProofSuit.Patches
         public static void Postfix(ref Player __instance, string slot, InventoryItem item)
         {
             Equipment equipment = Inventory.main.equipment;
-            /*int num = __instance.equipmentModels.Length;
-            for (int i = 0; i < num; i++)
+            int num = __instance.equipmentModels.Length;
+            for(int i = 0; i < num; i++)
             {
                 Player.EquipmentType equipmentType = __instance.equipmentModels[i];
-            */
-            foreach(Player.EquipmentType equipmentType in __instance.equipmentModels)
-            {
                 TechType techTypeInSlot = equipment.GetTechTypeInSlot(equipmentType.slot);
                 if (techTypeInSlot == Main.suitPrefab.TechType)
                     techTypeInSlot = TechType.ReinforcedDiveSuit;
                 else if (techTypeInSlot == Main.glovesPrefab.TechType)
                     techTypeInSlot = TechType.ReinforcedGloves;
                 else
-                    continue;
+                  continue;
 
                 bool flag = false;
-                //int j = 0;
-                //int num2 = equipmentType.equipment.Length;
-                //while (j < num2)
-                //{
-                //    Player.EquipmentModel equipmentModel = equipmentType.equipment[j];
-                foreach (Player.EquipmentModel equipmentModel in equipmentType.equipment)
-                { 
+                int j = 0;
+                int num2 = equipmentType.equipment.Length;
+                while (j < num2)
+                {
+                    Player.EquipmentModel equipmentModel = equipmentType.equipment[j];
                     bool flag2 = equipmentModel.techType == techTypeInSlot;
                     flag = (flag || flag2);
                     if (equipmentModel.model)
                     {
                         equipmentModel.model.SetActive(flag2);
                     }
-                    //j++;
+                    j++;
                 }
                 if (equipmentType.defaultModel)
                 {
                     equipmentType.defaultModel.SetActive(!flag);
                 }
+                i++;
             }
 
             // Actually we don't need this, since we're not doing anything that would change the outcome of UpdateReinforcedSuit.
@@ -196,16 +161,15 @@ namespace AcidProofSuit.Patches
     [HarmonyPatch(typeof(Player), "UpdateReinforcedSuit")]
     internal class UpdateReinforcedSuitPatcher
     {
-        [HarmonyPostfix]
-        public static void Postfix(ref Player __instance)
+        [HarmonyPrefix]
+        public static bool Prefix(ref Player __instance)
         {
             if (__instance != null)
             {
                 if (Main.bInAcid)
                 {
                     // Player is currently immersed in acid; if this change changes their acid immunity, start/stop playing the effects
-                    Equipment e = Inventory.main.equipment;
-                    if (e.GetCount(Main.suitPrefab.TechType) > 0 && e.GetCount(Main.glovesPrefab.TechType) > 0 && e.GetCount(Main.helmetPrefab.TechType) > 0)
+                    if (Inventory.main.equipment.GetCount(Main.suitPrefab.TechType) > 0 && Inventory.main.equipment.GetCount(Main.glovesPrefab.TechType) > 0)
                     {
                         if (__instance.acidLoopingSound.playing)
                             __instance.acidLoopingSound.Stop();
@@ -218,7 +182,7 @@ namespace AcidProofSuit.Patches
                 }
             }
 
-            // return true;
+            return true;
         }
     }
 
@@ -232,14 +196,8 @@ namespace AcidProofSuit.Patches
 
             Main.bInAcid = true;
 
-            if (__instance != null)
-            {
-                Equipment e = Inventory.main.equipment;
-                if (e.GetCount(Main.suitPrefab.TechType) > 0
-                    && e.GetCount(Main.glovesPrefab.TechType) > 0
-                    && e.GetCount(Main.helmetPrefab.TechType) > 0)
+            if (__instance != null && Inventory.main.equipment.GetCount(Main.suitPrefab.TechType) > 0 && Inventory.main.equipment.GetCount(Main.glovesPrefab.TechType) > 0)
                 return false;
-            }
 
             return true;
         }
